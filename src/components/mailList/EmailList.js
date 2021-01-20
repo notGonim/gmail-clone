5ab167580339a55f5c6534e6b67187947d18a0d8
notGonim <mhmodgonim96@gmail.com>
2021-01-20 11:24:28 +0200

    Start :with email list

--- src/components/mailList/EmailList.js
+++ src/components/mailList/EmailList.js
@@ -1,12 +1,56 @@
+import { IconButton } from '@material-ui/core'
+import { CheckBox } from '@material-ui/icons'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import React from 'react'
+import './EmailList.scss'
+import { Section } from './Section';
+
+
 
 export const EmailList = () => {
     return (
         <div className="emailList">
             <div className="emailList_setting">
                 <div className="emailList_setting__left">
-                    
+                    <CheckBox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
+                <div className="emailList_setting_right">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="emailList_section">
+                <Section Icon={InboxIcon} title="Primary" color="red" selected />
+                <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+                <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
             </div>
         </div>
     )
---
+++ src/components/mailList/Section.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Section.scss'
+export const Section = ({ Icon, title, color, selected }) => {
+    return (
+        <div className={`section `}>
+
+        </div>
+    )
+}
